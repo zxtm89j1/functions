0e3a59a07dee8f03bfe7901c9525b80cbd9c4ffe
Spencer Bohol <zxtm89j1@gmail.com>
2022-08-29 16:33:16 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// const flight = 'LH1234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 235886621111,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr.' + passenger.name;
+
+//   if (passenger.passport === 235886621111) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //ES5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 5);
